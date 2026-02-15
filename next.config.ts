03d4b84c89702c7f7965493e70dbfeaa930f9ff4
ex-Plant <konradantonik@gmail.com>
2026-02-15 18:53:29 +0100

    test stale time experimental option

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
+  experimental: {
+    staleTimes: {
+      dynamic: 30,
+    },
+  },
   serverExternalPackages: ['payload', 'pino', 'pino-pretty', 'thread-stream'],
   images: {
     remotePatterns: [
